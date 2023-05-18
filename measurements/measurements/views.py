cc5858e1d7414969a81939df0ad91d2c214e0304
jfpatino01 <64269726+jfpatino01@users.noreply.github.com>
2023-05-17 21:30:18 -0500

    change

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -33,7 +33,7 @@ def MeasurementCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        if check_variable(data_json) == True and check_place(measurement) == True:
+        if check_variable(data_json) == True and check_place(data_json) == True:
             measurement = Measurement()
             measurement.variable = data_json['variable']
             measurement.value = data_json['value']
@@ -50,7 +50,7 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(measurement) == True and check_place(measurement) == True:
+                    if check_variable(measurement) == True and check_place(data_json) == True:
                         db_measurement = Measurement()
                         db_measurement.variable = measurement['variable']
                         db_measurement.value = measurement['value']
