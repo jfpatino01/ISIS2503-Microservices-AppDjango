706b2bdf4137c64d72475b43c8c447c3af4d4028
jfpatino01 <64269726+jfpatino01@users.noreply.github.com>
2023-05-17 22:20:05 -0500

    name

--- places/places/models.py
+++ places/places/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Places(models.Model):
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
-        return '%s %s' % (self.value, self.unit)
+        return '%s %s' % (self.name)
