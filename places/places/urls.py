c20d0289539f61c126701f2a995fc9e1544e8c7a
jfpatino01 <64269726+jfpatino01@users.noreply.github.com>
2023-05-17 21:51:43 -0500

    sos

--- places/places/urls.py
+++ places/places/urls.py
@@ -7,5 +7,4 @@ from . import views
 urlpatterns = [
     url(r'^places/', views.PlacesList),
     url(r'^placecreate/$', csrf_exempt(views.PlaceCreate), name='placeCreate'),
-    url(r'^createplaces/$', csrf_exempt(views.PlacesCreate), name='createPlaces'),
 ]
--- places/places/views.py
+++ places/places/views.py
@@ -8,49 +8,16 @@ from django.conf import settings
 import requests
 import json
 
-def check_variable(data):
-    r = requests.get(settings.PATH_VAR, headers={"Accept":"application/json"})
-    variables = r.json()
-    for variable in variables:
-        if data["variable"] == variable["id"]:
-            return True
-    return False
-
 def PlacesList(request):
     queryset = Places.objects.all()
-    context = list(queryset.values('id', 'variable', 'value', 'unit', 'place', 'dateTime'))
+    context = list(queryset.values('id', 'name'))
     return JsonResponse(context, safe=False)
 
 def PlaceCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        if check_variable(data_json) == True:
-            place = Places()
-            place.variable = data_json['variable']
-            place.value = data_json['value']
-            place.unit = data_json['unit']
-            place.place = data_json['place']
-            place.save()
-            return HttpResponse("successfully created place")
-        else:
-            return HttpResponse("unsuccessfully created place. Variable does not exist")
-
-def PlacesCreate(request):
-    if request.method == 'POST':
-        data = request.body.decode('utf-8')
-        data_json = json.loads(data)
-        places_list = []
-        for place in data_json:
-                    if check_variable(place) == True:
-                        db_place = Places()
-                        db_place.variable = place['variable']
-                        db_place.value = place['value']
-                        db_place.unit = place['unit']
-                        db_place.place = place['place']
-                        places_list.append(db_place)
-                    else:
-                        return HttpResponse("unsuccessfully created place. Variable does not exist")
-        
-        Places.objects.bulk_create(places_list)
-        return HttpResponse("successfully created places")
+        place = Places()
+        place.name = data_json['name']
+        place.save()
+        return HttpResponse("successfully created place")
