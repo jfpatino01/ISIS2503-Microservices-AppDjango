2fda1e1493ef90f887293a9048848732092b40f4
jfpatino01 <64269726+jfpatino01@users.noreply.github.com>
2023-05-17 22:13:33 -0500

    upsi

--- places/places/models.py
+++ places/places/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 class Places(models.Model):
-    variable = models.IntegerField(null=False, default=None)
-    value = models.FloatField(null=True, blank=True, default=None)
-    unit = models.CharField(max_length=50)
-    place = models.CharField(max_length=50)
-    dateTime = models.DateTimeField(auto_now_add=True)
-
+    name = models.CharField(max_length=50)
+    
     def __str__(self):
         return '%s %s' % (self.value, self.unit)
